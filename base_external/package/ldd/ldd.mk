0bb6855850acbb1671f18c7296b153fe188054f7
CeSiumUA <fedir.katushonok@gmail.com>
2024-04-11 23:04:52 +0300

    Updated package and commits

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '7415e51974f09fc6a8ce891545d904912bdf007e'
+AESD_ASSIGNMENTS_VERSION = 'efd7f21d89a7ef6dbbac4228fd65ac977aafb6a6'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-CeSiumUA.git'
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-CeSiumUA.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
