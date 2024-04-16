d997cc3230df40d95342e2feb647de57dfbc2b92
CeSiumUA <fedir.katushonok@gmail.com>
2024-04-16 07:25:36 +0300

    Updated commit of assignment-7

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-LDD_VERSION = 'efd7f21d89a7ef6dbbac4228fd65ac977aafb6a6'
+LDD_VERSION = 'c80f7355c19b49a18e87c9528600fcba0fad97c4'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -13,8 +13,8 @@ LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-CeSiumUA.git'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
-LDD_MODULE_SUBDIRS=misc-modules
-LDD_MODULE_SUBDIRS+=scull
+LDD_MODULE_SUBDIRS = misc-modules
+LDD_MODULE_SUBDIRS += scull
 
 define LDD_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
